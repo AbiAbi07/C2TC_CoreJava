b94f7992b99c65cef0518b86f70123b7b0b3afdc
ADMIN <ADMIN@DESKTOP-VMKUBGK>
2024-09-04 12:25:43 +0530

    This is my first commit

---
+++ C2TC_abi/src/com/tns/day1/loop.java
@@ -0,0 +1,29 @@
+package com.tns.day1;
+
+public class loop {
+
+    public static void main(String[] args) {
+        
+        // For loop
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+        // While loop
+        System.out.println("While Loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("j = " + j);
+            j++;
+        }
+
+        // Do-while loop
+        System.out.println("Do-While Loop:");
+        int k = 1;
+        do {
+            System.out.println("k = " + k);
+            k++;
+        } while (k <= 5);
+    }
+}
